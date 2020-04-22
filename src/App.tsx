d5efe47479816b84d50bebd742753700eff4dffa
satoooon8888 <samidare256@gmail.com>
2020-04-22 16:21:11 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// In App.js in a new project
+
+import React from 'react';
+import {View, Text} from 'react-native';
+export const App = () => {
+  return (
+    <View>
+      <Text>Hello!</Text>
+    </View>
+  );
+};
